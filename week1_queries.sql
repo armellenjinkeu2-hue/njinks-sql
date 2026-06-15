4630e02c572bb49b8b0026587dda99aaca299529
armellenjinkeu2-hue <armellenjinkeu2@gmail.com>
2026-06-14 19:21:39 -0500

    Update week1_queries.sql

--- week1_queries.sql
+++ week1_queries.sql
@@ -1,5 +1,21 @@
-A LIKE query
-A BETWEEN query
-An IN query
-A CONCAT query
-A math query like ROUND(gdp/population, -3)
+-- Query 1: Pattern matching with LIKE
+SELECT name FROM world
+WHERE name LIKE '%United%'
+
+-- Query 2: Range filter with BETWEEN
+SELECT name, area FROM world
+WHERE area BETWEEN 200000 AND 250000
+
+-- Query 3: Filter by list with IN
+SELECT name, population FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
+
+-- Query 4: Math with ROUND
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp >= 1000000000000
+
+-- Query 5: CONCAT and pattern matching
+SELECT name, capital FROM world
+WHERE capital LIKE CONCAT(name, '%')
+AND capital <> name
